f4746919ff865e73ac0cb90d4777ef4baa4d7a56
Teo <75574673+sunsakis@users.noreply.github.com>
2025-10-27 01:30:44 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 828, 1200, 1920, 2560, 3840],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1536, 1920, 2560, 3840],
     imageSizes: [64, 128, 256, 384],
     minimumCacheTTL: 60,
   },
